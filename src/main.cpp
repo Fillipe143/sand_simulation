b828bc540ab9d96232f4df1f3ccab9720e83b22d
Fillipe143 <fillipedev.contato@gmail.com>
2024-05-03 18:42:57 -0300

    created base raylib window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
+#include <raylib.h>
+
+const int screenWidth = 800;
+const int screenHeight = 800;
+const int targetFPS = 60;
+
+void loop();
+
 int main() {
+    SetTraceLogLevel(LOG_WARNING);
+    InitWindow(screenWidth, screenHeight, "SandSimulation");
+    SetTargetFPS(targetFPS);
+
+    while (!WindowShouldClose()) loop();
+    CloseWindow();
+
     return 0;
+}
+
+void loop() {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    EndDrawing();
 }
