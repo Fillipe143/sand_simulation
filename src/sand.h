dd827dad4b1f96d8356119201da25bc20b5c83fc
Fillipe143 <fillipedev.contato@gmail.com>
2024-05-03 22:14:50 -0300

    removed unused files

--- src/sand.h
+++
@@ -1,9 +0,0 @@
-#include <raylib.h>
-
-struct GrainOfSand {
-    Vector2 pos, spd;
-    Color color;
-};
-
-GrainOfSand newGrainOfSand(float x, float y, Color color);
-void drawGrainOfSand(GrainOfSand grain);
